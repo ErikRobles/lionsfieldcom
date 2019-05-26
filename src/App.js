9aeb3432da23d1154bc292494f32e417234c69d4
ErikRobles <erikjames69@hotmail.com>
2019-05-25 20:24:04 -0500

    added history

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Contacto from './pages/Contacto';
 class App extends Component {
   render() {
     return (
-      <Router basename="lionsfieldcom"> 
+      <Router  history={history} basename={process.env.PUBLIC_URL}> 
         <div>
         <Route exact path='/' component={Inicio} />
         <Route exact path='/Gamification' component={Gamification} />
