508bf449625fc48b49db02de8dbb94f394de1411
ErikRobles <erikjames69@hotmail.com>
2019-05-25 20:13:16 -0500

    added basename again

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Contacto from './pages/Contacto';
 class App extends Component {
   render() {
     return (
-      <Router> 
+      <Router basename="lionsfieldcom"> 
         <div>
         <Route exact path='/' component={Inicio} />
         <Route exact path='/Gamification' component={Gamification} />
